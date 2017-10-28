2a895a61e7776280f084ff112a9a927a1e9f598a
Bede Kelly <bedekelly97@gmail.com>
2017-10-28 13:32:32 +0100

    Parallel rendering w/ frame interleaving

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <float.h>
 #include <complex.h>
+#include <math.h>
 
 // Default resolution.
-#define MAX_ITERATIONS 255
+#define MAX_ITERATIONS 350
 
-// double CENTRE_X = -0.722222;
-// double CENTRE_Y = 0;
-// double CENTRE_X = -1.7400623825;
-// double CENTRE_Y = 0.028175339779;
+// The closer this is to 1, the more *slowly* this will zoom.
+// Use `log(2, multiplier^numFrames)<sizeof(long double)` to work out if we'll hit rounding errors.
+#define ZOOM_MULTIPLIER ((long double) 0.9525)
 
-double CENTRE_X = -0.74834786263068993619;
-double CENTRE_Y = -0.063037465497871903373;
+// Which coordinates should we zoom in on?
+long double CENTRE_X = 0.3994999999988, CENTRE_Y = 0.195303;
+
 
 enum COLOR {
   RED, GREEN, BLUE
@@ -19,43 +21,44 @@ enum COLOR {
 
 
 /* Calculate the number of iterations a point takes to leave a bound. */
-int iterations(double cr, double ci, int max_it) {
+int iterations(long double cr, long double ci, int max_it) {
   int i = 0;
-  double complex z = 0 + 0*I;
-  double complex c = cr + ci*I;
+  long double complex z = 0 + 0*I;
+  long double complex c = cr + ci*I;
   for (i=0; i<max_it; i++) {
     z = z*z + c;
-    if (cabs(z) > 4)
+    if (cabsl(z) > 4)
       break;
   }
+  // printf("%d,", i);
   return i;
 }
 
 
 /* Linearly map a value from one range into another. */
-double linmap(double val, double lower1, double upper1, double lower2, double upper2) {
+long double linmap(long double val, long double lower1, long double upper1, long double lower2, long double upper2) {
   return ((val - lower1) / (upper1 - lower1)) * (upper2 - lower2) + lower2;
 }
 
 
 /* Calculate the colour for coordinates (x, y) and write into color[]. */
-void get_color(int x, int y, int maxX, int maxY, double ZOOM, unsigned char color[]) {
+void get_color(int x, int y, int maxX, int maxY, long double ZOOM, unsigned char color[]) {
 
-  double ASPECT_RATIO = (double)maxX / (double)maxY;
-  double X_LOWER = (CENTRE_X - (ASPECT_RATIO * ZOOM));
-  double X_UPPER = (CENTRE_X + (ASPECT_RATIO * ZOOM));
-  double Y_LOWER = (CENTRE_Y - ZOOM);
-  double Y_UPPER = (CENTRE_Y + ZOOM);
+  long double ASPECT_RATIO = (double)maxX / (double)maxY;
+  long double X_LOWER = (CENTRE_X - (ASPECT_RATIO * ZOOM));
+  long double X_UPPER = (CENTRE_X + (ASPECT_RATIO * ZOOM));
+  long double Y_LOWER = (CENTRE_Y - ZOOM);
+  long double Y_UPPER = (CENTRE_Y + ZOOM);
 
   // Calculate the viewport of our render.
-  double x1 = linmap(x, 0, maxX, X_LOWER, X_UPPER);
-  double y1 = linmap(y, 0, maxY, Y_LOWER, Y_UPPER);
+  long double x1 = linmap(x, 0, maxX, X_LOWER, X_UPPER);
+  long double y1 = linmap(y, 0, maxY, Y_LOWER, Y_UPPER);
 
   // How many iterations before this complex value "escapes" our bound of 4?
   int its = iterations(x1, y1, MAX_ITERATIONS);
 
   // Normalise number of iterations to a value in [0, 1].
-  double t = (double) its / (double) MAX_ITERATIONS;
+  long double t = (long double) its / (long double) MAX_ITERATIONS;
 
   // Calculate values for Bernstein Polynomials in [0, 255] interval.
   color[RED] = linmap(8.5 * (1-t) * (1-t) * (1-t) * t, 0, 1, 0, 255);
@@ -96,16 +99,27 @@ void generate_file(int dimx, int dimy, double ZOOM, int frameNo) {
 
 
 int main(int argc, char *argv[]) {
+  // Grab our parameters from the system args.
   int dimx = atoi(argv[1]);
   int dimy = atoi(argv[2]);
   int secs = atoi(argv[3]);
   int fps = atoi(argv[4]);
+  int i = atoi(argv[5]);
+  int cores = atoi(argv[6]);
 
-  double ZOOM = 2;
-  
-  for (int i=0; i<secs*fps; i++) {
+  // Initialise the zoom using i to properly interpolate frames.
+  long double ZOOM = DBL_EPSILON / pow(ZOOM_MULTIPLIER, i);
+
+  // i should be incremented by the number of parallel processes we have.
+  for (; i<secs*fps; i+=cores) {
+    // Write a frame with the current zoom.
     generate_file(dimx, dimy, ZOOM, i);
-    ZOOM *= 0.97;
+
+    // Only render every `cores` zoom level.
+    ZOOM /= (pow(ZOOM_MULTIPLIER, cores));
+
+
+    // Get some friendly logging!
     if (i % (fps/4) == 0)
       printf("%.2f%% done!\n", 100 * (double)i/(secs*fps));
   }
--- zoom.sh
+++ zoom.sh
@@ -1,22 +1,31 @@
-DURATION=30  #seconds
+DURATION=25  #seconds
 FRAMES_PER_SECOND=30
 FILENAME=`date '+%Y-%m-%d_%H:%M:%S'`
-WIDTH=1920
-HEIGHT=1080
+WIDTH=3840
+HEIGHT=2160
 RESOLUTION=${WIDTH}x$HEIGHT
 
+# Cleanup the frames directory.
 mkdir -p frames &&
+    mkdir -p videos &&
     make main &&
-    rm -f frames/*.ppm && 
-    time ./main 1920 1080 $DURATION $FRAMES_PER_SECOND &&
-    cd frames &&
-    
-    time ffmpeg -framerate $FRAMES_PER_SECOND \
-	 -i frame-%d.ppm    \
-	 -s:v $RESOLUTION   \
-	 -c:v libx264       \
-	 -pix_fmt yuv420p   \
-	 -profile:v high444 \
-	 -crf 20 $FILENAME.mp4 &&
+    rm -f frames/*.ppm
 
+# Process alternating frames in parallel and wait for all to complete.
+./main $WIDTH $HEIGHT $DURATION $FRAMES_PER_SECOND 0 4 & P1=$!
+./main $WIDTH $HEIGHT $DURATION $FRAMES_PER_SECOND 1 4 & P2=$!
+./main $WIDTH $HEIGHT $DURATION $FRAMES_PER_SECOND 2 4 & P3=$!
+./main $WIDTH $HEIGHT $DURATION $FRAMES_PER_SECOND 3 4 & P4=$!
+wait $P1 $P2 $P3 $P4
+
+# Even if we only render some of the frames, stitch the video together.
+cd frames &&
+    time ffmpeg -framerate $FRAMES_PER_SECOND \
+         -i frame-%d.ppm    \
+         -s:v $RESOLUTION   \
+         -c:v libx264       \
+         -pix_fmt yuv420p   \
+         -profile:v high444 \
+         -crf 20 ../videos/$FILENAME.mp4 &&
+    cd ../videos &&
     open $FILENAME.mp4
